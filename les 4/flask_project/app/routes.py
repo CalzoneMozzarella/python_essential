fa007aff8dbd54659c52bf508f77ede89427439c
o.shalashnyi <don.alexsh@gmail.com>
2021-11-29 21:36:26 +0200

    les 4

--- les 4/flask_project/app/routes.py
+++ les 4/flask_project/app/routes.py
@@ -1,10 +1,16 @@
 from app import app, forms
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route('/')
-@app.route('/index', methods=['POST'])
+@app.route('/index', methods=['GET','POST'])
 def index():
     title = 'Login page'
-    form =forms.EnterNameForm()
+    form = forms.EnterNameForm()
+
+    data = request.form.get('user_name')
+    print(type(data))
+    print(data)
+
     return render_template('index.html', title=title, form=form)
+
