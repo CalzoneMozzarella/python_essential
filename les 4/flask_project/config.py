8961945bef6fbed0d81894b93f3ae6907c85c4b0
o.shalashnyi <don.alexsh@gmail.com>
2021-12-15 15:27:34 +0200

    les 4

--- les 4/flask_project/config.py
+++ les 4/flask_project/config.py
@@ -1,4 +1,11 @@
-import  os
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hyctctjcvbliuhiy12384kh'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
