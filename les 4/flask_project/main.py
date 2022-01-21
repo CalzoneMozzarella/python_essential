e6e9265cc1ef052f1c1ab1c99303bd401cc94be0
o.shalashnyi <don.alexsh@gmail.com>
2022-01-21 08:46:23 +0200

    les 4 add password hash

--- les 4/flask_project/main.py
+++ les 4/flask_project/main.py
@@ -2,4 +2,10 @@ import app
 
 if __name__ == '__main__':
     app.app.run(debug=True)
-   
+
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
